92493aed212546c848fd36fe6f040b7f422fb5cb
higekick <higekick39@gmail.com>
2022-03-17 13:57:16 +0900

    finish Chapter 6 Persistence and Concurrency https://developer.apple.com/tutorials/app-dev-training/handling-errors

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct DailyScrum: Identifiable{
+struct DailyScrum: Identifiable, Codable{
     let id: UUID
 //    var data: Data
     var title: String
@@ -33,7 +33,7 @@ struct DailyScrum: Identifiable{
 }
 
 extension DailyScrum {
-    struct Attendee: Identifiable{
+    struct Attendee: Identifiable, Codable{
         let id: UUID
         var name: String
         
